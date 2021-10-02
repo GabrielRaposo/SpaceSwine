d6e0d0c8606a8960bdc25a118978bd7786e21f5a
GabrielRaposo <gabriel.s.raposo@gmail.com>
2021-10-02 00:19:28 -0300

    Criação da Round Session Data

---
+++ Assets/Scripts/Round/RoundSessionData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "Round Session", menuName = "ScriptableObjects/Round Session")]
+public class RoundSessionData : ScriptableObject
+{
+    public int startingIndex;
+    public int lastIndex;
+    public UnityAction OnSessionCompleted;
+}
