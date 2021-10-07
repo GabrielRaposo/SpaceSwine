67f288c8e7cc33180b7b68a59fc7bde2f7ff99a8
GabrielRaposo <gabriel.s.raposo@gmail.com>
2021-10-07 15:55:14 -0300

    Testes visuais

---
+++ Assets/Scripts/Minigame/Jumper/MJ_Planet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jumper
+{
+    public class MJ_Planet : MonoBehaviour
+    {
+        [SerializeField] Transform rotationAnchor;
+        [SerializeField] float rotationSpeed;
+
+        Transform current;
+
+        private void Start() 
+        {
+            if (!rotationAnchor)
+            {
+                enabled = false;
+                return;
+            }
+
+            SpriteRenderer spriteRenderer = rotationAnchor.GetComponent<SpriteRenderer>();
+            if (spriteRenderer)
+            {
+                spriteRenderer.flipX = rotationSpeed < 0;
+            }
+        }
+
+        public void Attach (Transform current)
+        {
+            current.SetParent(rotationAnchor);
+            current.position = rotationAnchor.position + Vector3.up * .95f;
+
+            this.current = current;
+        }
+
+        private void FixedUpdate() 
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.fixedDeltaTime);
+            //rotationAnchor.Rotate(Vector3.forward * rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame/Jumper/MJ_Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jumper
+{
+    public class MJ_Player : MonoBehaviour
+    {
+        bool attached;
+
+        void Start()
+        {
+        
+        }
+
+        private void OnTriggerStay2D (Collider2D collision) 
+        {
+            if (!attached)
+            {
+                MJ_Planet planet = collision.GetComponent<MJ_Planet>();
+                if (!planet)
+                    return;
+
+                planet.Attach (transform);
+                attached = true;
+            }
+        }
+
+    }
+}
