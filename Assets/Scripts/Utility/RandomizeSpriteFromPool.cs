70e99f47bf9530a278abf16e27a337cee52e92e3
GabrielRaposo <gabriel.s.raposo@gmail.com>
2021-10-17 13:46:02 -0300

    Sprites de NPCs e elementos de cenário adicionados ao projeto (#16)
    * Update ExplorationScene.unity
    
    * Robozão, Tree e Leg adicionados
    
    + Flores e Grama
    
    * NPCs de Mundo 1 implementados

---
+++ Assets/Scripts/Utility/RandomizeSpriteFromPool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomizeSpriteFromPool : MonoBehaviour
+{
+    [SerializeField] List<Sprite> sprites;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    [SerializeField] private bool hasBeenSetup;    
+
+    void OnValidate()
+    {
+        return;
+
+        if (hasBeenSetup)
+            return;
+
+        if (sprites == null || sprites.Count < 1)
+            return;
+
+        if (!spriteRenderer)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        if (!spriteRenderer)
+            return;
+
+        Setup();
+    }
+
+    private void Setup()
+    {
+        int index = Random.Range(0, sprites.Count);
+        spriteRenderer.sprite = sprites[index % sprites.Count];
+
+        hasBeenSetup = true;
+    }
+}
