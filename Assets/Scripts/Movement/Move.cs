7f5058bb3ba180175433122e5388f4cf774d0755
Jerry123i <igorsc.jerry@gmail.com>
2021-11-07 17:17:31 -0300

    Script de movimento

--- Assets/Scripts/Movement/Move.cs
+++ Assets/Scripts/Movement/Move.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] float duration;
+    [SerializeField] bool stay;
+
+    int direction;
+    float count;
+
+    private float distance;
+    private Vector3 normalizedDirection;
+    
+    //TODO Reset position on level reset
+    
     void Start()
     {
+        direction = 1; 
+        count = 0;
+
+        if (duration == 0)
+            duration = float.MinValue;
         
+        distance = ( targetPosition - transform.position).magnitude;
+        normalizedDirection = (targetPosition - transform.position).normalized;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        float step = (distance / duration) * direction * Time.fixedDeltaTime;
+        transform.localPosition += normalizedDirection * step;
+
+        count += Time.fixedDeltaTime;
+        if (count >= duration)
+        {
+            if (stay)
+            {
+                this.enabled = false;
+                return;
+            }
+                
+            direction *= -1;
+            count = 0;
+        }
     }
 }
