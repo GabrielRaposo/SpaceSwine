8e902e7120b979a921dc48cb8ff8465e81ae9cf2
GabrielRaposo <gabriel.s.raposo@gmail.com>
2021-11-11 20:06:11 -0300

    Base de coletável criada

---
+++ Assets/Scripts/Currency/MoveToTarget.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveToTarget : MonoBehaviour
+{
+    [SerializeField] float duration;
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] bool turnOffOnReach;
+
+    float t;
+    Vector2 startingPosition;
+
+    Transform target;
+    UnityAction OnReachTarget;
+
+    public void SetTarget(Transform target, UnityAction OnReachTarget)
+    {
+        this.target = target;
+        this.OnReachTarget = OnReachTarget; 
+
+        startingPosition = transform.position;
+        t = 0;
+        enabled = true;
+    }
+
+    void Update()
+    {
+        if (!target)
+            return;
+
+        t += Time.deltaTime;
+        if (t > duration)
+            t = duration;
+
+        transform.position = Vector2.Lerp(startingPosition, target.position, t / duration);
+
+        if (t == duration)
+        {
+            OnReachTarget?.Invoke();
+            if (turnOffOnReach)
+            {
+                target = null;
+                enabled = false;
+            }
+        }
+    }
+}
