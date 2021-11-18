9c4acafc2a9bab2d0deead32c2012d8f37830751
GabrielRaposo <gabriel.s.raposo@gmail.com>
2021-11-17 22:53:32 -0300

    StoryEvents de teste criados e base do NPC Spawner

---
+++ Assets/Scripts/NPC/NPCSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public struct NPCRule
+    {
+        public GameObject instance;
+        public bool[] criteria;
+    }
+
+    [SerializeField] List<StoryEventScriptableObject> storyEvents;
+    [SerializeField] List<NPCRule> rules;
+
+    GameObject currentActive;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnValidate() 
+    {
+        if (storyEvents == null || storyEvents.Count < 1 || rules == null)
+            return;
+
+        for (int i = 0; i < rules.Count; i++) 
+        {
+            NPCRule rule = rules[i];
+            if (rule.criteria.Length != storyEvents.Count)
+                rule.criteria = new bool[storyEvents.Count];
+        }
+    }
+}
