1f21c18f8148dc367b4a837d876861483cbbbd05
Jerry123i <igorsc.jerry@gmail.com>
2022-01-12 12:20:53 -0300

    Level editor tools

---
+++ Assets/Scripts/LevelBuildingTools/CircleRuler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRuler : MonoBehaviour
+{
+    public List<Transform> objects;
+    public float radius;
+    public float offsetRotation;
+    public float sector;
+    public bool rotateRadial;
+
+    private void OnValidate()
+    {
+        if (objects == null) objects = new List<Transform>();
+        if(objects.Count==0) return;
+
+        if (radius <= 0) radius = 0f;
+
+        var part = (360f-sector) / objects.Count;
+        
+        for (int i = 0; i < objects.Count; i++)
+        {
+            float angle = offsetRotation + part * i;
+            var vec = Mathg.AngleToDirection(angle);
+            objects[i].transform.position = transform.position + vec * radius;
+
+            if (rotateRadial)
+                objects[i].transform.eulerAngles = new Vector3(0f, 0f, angle-90f);
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelBuildingTools/LinearSequenceTool.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LinearSequenceTool : MonoBehaviour
+{
+    public List<ObjectPoint> objects;
+    public List<TurningPoint> turningPoints;
+
+    public void OnValidate()
+    {
+        if (objects == null) objects = new List<ObjectPoint>();
+        foreach (ObjectPoint objectPoint in objects)
+        {
+            if(objectPoint.obj)
+                objectPoint.obj.transform.position = objectPoint.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+
+        for (int i = 0; i < turningPoints.Count-1; i++)
+        {
+            Gizmos.color = Color.green;
+            var nextPointPos = turningPoints[i+1].position;
+            
+            Gizmos.DrawLine(turningPoints[i].position, nextPointPos);
+            
+            
+            //var dir = nextPointPos - turningPoints[i].position;
+            var dir = Vector3.up;
+            var angle = Mathg.VectorToAngle(dir)+turningPoints[i].angle;
+            
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(nextPointPos, nextPointPos+ Mathg.AngleToDirection(angle));
+            
+            Gizmos.color = new Color(0.45f,0.45f,1f);
+            Gizmos.DrawLine(nextPointPos, nextPointPos+ Mathg.AngleToDirection(angle+180f));
+
+        }
+    }
+}
+
+
+
+[System.Serializable]
+public class ObjectPoint
+{
+    public Vector3 position;
+    public GameObject obj;
+}
+
+[System.Serializable]
+public class TurningPoint
+{
+    public Vector3 position;
+    [Range(0f,360f)]public float angle;
+}
