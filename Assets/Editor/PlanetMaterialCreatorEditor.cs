4c11b118869b72ea94b5cc8c8cb84e82d6caad8c
Jerry123i <igorsc.jerry@gmail.com>
2022-01-29 12:34:33 -0300

    Planet material editor

---
+++ Assets/Editor/PlanetMaterialCreatorEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(PlanetMaterialCreator))]
+public class PlanetMaterialCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        PlanetMaterialCreator obj = target as PlanetMaterialCreator;
+        
+        //base.OnInspectorGUI();
+
+        GUILayout.BeginHorizontal();
+        
+        obj.baseMaterial = (Material)EditorGUILayout.ObjectField("Base Material", obj.baseMaterial, typeof(Material));
+
+        GUIStyle myButtonStyle = new GUIStyle(GUI.skin.button);
+        myButtonStyle.fontSize = 13;
+
+        myButtonStyle.hover.textColor = new Color(0.2f, 0.8f, 0.4f);
+
+        myButtonStyle.fixedWidth = 40;
+        
+        if(GUILayout.Button("⇌", myButtonStyle))
+            obj.ClarMaterial();
+        
+        GUILayout.EndHorizontal();
+        
+        obj.currentMaterial = (Material)EditorGUILayout.ObjectField("Material", obj.currentMaterial, typeof(Material));
+
+
+    }
+}
---
+++ Assets/Scripts/ShaderScripts/PlanetMaterialCreator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlanetMaterialCreator : MonoBehaviour
+{
+    public Material baseMaterial;
+    public Material currentMaterial;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private void OnValidate()
+    {
+        if(gameObject.scene.name == "") return;
+        if(gameObject.scene.name == gameObject.name) return;
+    
+        if (currentMaterial == null)
+        {
+            if(baseMaterial == null) return;
+            
+            currentMaterial = new Material(baseMaterial);
+            currentMaterial.name = currentMaterial.name + " " + Random.Range(0, 9999).ToString("X");
+            TweakMaterial(currentMaterial);
+            
+            if(_spriteRenderer == null) return;
+
+            _spriteRenderer.material = currentMaterial;
+        }
+            
+    }
+
+    public void ClarMaterial()
+    {
+        currentMaterial = null;
+        OnValidate();
+    }
+    
+    private void TweakMaterial(Material mat)
+    {
+        //TweakVectorValue(mat, "_SphereOffset", v => new Vector3(Random.Range(0f,999f),Random.Range(0f,999f),Random.Range(0f,999f)));
+        
+        TweakFloatValue(mat, "_SpotsCount1", f => f * Random.Range(0.8f,1.2f));
+        TweakFloatValue(mat, "_SpotsCount2", f => f * Random.Range(0.8f,1.2f));
+        TweakFloatValue(mat, "_SpotsCount3", f => f * Random.Range(0.8f,1.2f));
+        //
+        TweakFloatValue(mat, "_SpotsSize1", f => f * Random.Range(0.8f,1.2f));
+        TweakFloatValue(mat, "_SpotsSize2", f => f * Random.Range(0.8f,1.2f));
+        TweakFloatValue(mat, "_SpotsSize3", f => f * Random.Range(0.8f,1.2f));
+        //
+        TweakFloatValue(mat, "_NoiseRotation1", f => Random.Range(0f,360f));
+        TweakFloatValue(mat, "_NoiseRotation2", f => Random.Range(0f,360f));
+        TweakFloatValue(mat, "_NoiseRotation3", f => Random.Range(0f,360f));
+        //
+        TweakVectorValue(mat, "_SpotsShape1", v => v*Random.Range(0.75f,1.25f));
+        TweakVectorValue(mat, "_SpotsShape2", v => v*Random.Range(0.75f,1.25f));
+        TweakVectorValue(mat, "_SpotsShape3", v => v*Random.Range(0.75f,1.25f));
+        //
+        TweakVectorValue(mat, "_NoiseOffset1", v => new Vector3(Random.Range(0f,999f),Random.Range(0f,160f),Random.Range(0f,160f)));
+        TweakVectorValue(mat, "_NoiseOffset2", v => new Vector3(Random.Range(0f,999f),Random.Range(0f,160f),Random.Range(0f,160f)));
+        TweakVectorValue(mat, "_NoiseOffset3", v => new Vector3(Random.Range(0f,999f),Random.Range(0f,160f),Random.Range(0f,160f)));
+        
+    }
+
+    private void TweakFloatValue(Material mat, string valueName, Func<float,float> action)
+    {
+        mat.SetFloat(valueName, action(mat.GetFloat(valueName)));
+    }
+
+    private void TweakVectorValue(Material mat, string valueName, Func<Vector4, Vector4> action)
+    {
+        mat.SetVector(valueName, action(mat.GetVector(valueName)));
+    }
+    
+}
