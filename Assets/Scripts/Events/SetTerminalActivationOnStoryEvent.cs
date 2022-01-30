fdc4c6ba8ee99d028025493bc346450cd67a6c3a
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-01-30 18:32:17 -0300

    Primeiros ajustes no mundo 1 (#43)
    * Adição da Direction Arrow
    
    * Update World1DangerScene.unity
    
    * Começo da criação do Mundo 1
    
    * Puzzle de acesso às fases de perigo criado
    
    * Ajuste de valores

---
+++ Assets/Scripts/Events/SetTerminalActivationOnStoryEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTerminalActivationOnStoryEvent : MonoBehaviour
+{
+    [SerializeField] StoryEventScriptableObject storyEvent;
+
+    void OnEnable()
+    {
+        if (!storyEvent)
+            return;
+
+        storyEvent.OnStateChange += OnStateChange;
+
+        OnStateChange (storyEvent.state);
+    }
+
+    private void OnStateChange (bool state)
+    {
+        Interactable interactable = GetComponentInChildren<Interactable>();
+        if (!interactable)
+            return;
+
+        interactable.SetInteraction(state);
+    }
+
+    private void OnDisable() 
+    {
+        if (!storyEvent)
+            return;
+
+        storyEvent.OnStateChange -= OnStateChange;
+    }
+}
