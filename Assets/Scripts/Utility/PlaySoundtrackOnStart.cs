6eea0a2dd03ad47ad21961f4635e049cd2f44dbd
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-01-31 15:03:17 -0300

    Funções de start de music em cena e loop Hard Coded

---
+++ Assets/Scripts/Utility/PlaySoundtrackOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundtrackOnStart : MonoBehaviour
+{
+    void Start()
+    {
+        SoundtrackManager soundtrackManager = SoundtrackManager.Instance;
+        if (!soundtrackManager)
+            return;
+
+        //Debug.Log("Play");
+        if (!SoundtrackManager.IsPlaying)
+            soundtrackManager.PlayMusic(skipPlay: true);
+    }
+}
