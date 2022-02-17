9fda27001462b8d9e414b376c53faedf8c51f870
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-02-17 17:14:12 -0300

    Reconhecimento inteligente de posicionamento do player com o SpaceLauncher

--- Assets/Scripts/SpaceLauncher.cs
+++ Assets/Scripts/SpaceLauncher.cs
@@ -6,16 +6,25 @@ public class SpaceLauncher : MonoBehaviour
 {
     [SerializeField] GameObject visualComponent;
 
-    bool interactable;
+    bool interactable = true;
 
     void Start()
     {
-        
+
     }
 
-    private void OnTriggerEnter2D (Collider2D collision) 
+    private void OnTriggerStay2D (Collider2D collision) 
     {
         if (!interactable)
+            return;
+
+        Vector2 direction = transform.position - collision.transform.position;
+        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        Vector2 anchoredDirection = RaposUtil.RotateVector(direction, angle);
+
+        //float distance = Vector2.Distance(collision.transform.position, transform.position);
+        //Debug.Log("distance: " + distance);
+        if (anchoredDirection.y > .15f)
             return;
 
         if (collision.CompareTag("Player"))
@@ -25,10 +34,11 @@ public class SpaceLauncher : MonoBehaviour
                 return;
 
             spaceJumper.transform.position = transform.position;
-            spaceJumper.LaunchIntoDirection(Vector2.right);
+            spaceJumper.RedirectIntoDirection(Vector2.right);
             UpdateVisualState(false);
 
             interactable = false;
+            Debug.Log("Called event!!!!");
         }
     }
 
