875fc7d320e39aa5671b115cbe852d01a47dbde1
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-02-17 16:33:09 -0300

    Base do Space Launcher

---
+++ Assets/Scripts/SpaceLauncher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceLauncher : MonoBehaviour
+{
+    [SerializeField] GameObject visualComponent;
+
+    bool interactable;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision) 
+    {
+        if (!interactable)
+            return;
+
+        if (collision.CompareTag("Player"))
+        {
+            SpaceJumper spaceJumper = collision.GetComponent<SpaceJumper>();
+            if (!spaceJumper)
+                return;
+
+            spaceJumper.transform.position = transform.position;
+            spaceJumper.LaunchIntoDirection(Vector2.right);
+            UpdateVisualState(false);
+
+            interactable = false;
+        }
+    }
+
+    private void UpdateVisualState(bool value)
+    {
+        visualComponent?.SetActive(value);
+    }
+}
