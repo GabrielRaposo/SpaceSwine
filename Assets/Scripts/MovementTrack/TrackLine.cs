1a5e43ddf640c83a72efc424f1d25d1d84e416c2
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-02-24 14:10:03 -0300

    Alterações no mundo 1 (#52)
    * Mudanças locais
    
    * Base do Space Launcher
    
    * Reconhecimento inteligente de posicionamento do player com o SpaceLauncher
    
    * Ajustes nos valores de encontro com o Space Booster
    
    * Testes com Space Boosters
    
    * Ajustes e funcionalidades para o Space Booster
    
    * Base da Sessão 2 do World 1 criada
    
    * Update World1ExplorationScene.unity
    
    * Base de Skipped Line
    
    * Base de Movement Track criado

---
+++ Assets/Scripts/MovementTrack/TrackLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackLine : MonoBehaviour
+{
+    public void Set (Vector2 pointA, Vector2 pointB)
+    {
+        transform.position = pointA;
+
+        Vector2 direction = pointB - pointA;
+        SpriteRenderer _renderer = GetComponent<SpriteRenderer>();
+        _renderer.size = new Vector2(_renderer.size.x, direction.magnitude);
+
+        float rotation = Vector2.SignedAngle(Vector2.up, direction);
+        transform.eulerAngles = Vector3.forward * rotation;
+    }
+}
