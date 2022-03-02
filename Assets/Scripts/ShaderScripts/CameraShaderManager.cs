081efbc01735d9f1d8929db334587c9f8a6e6c66
Jerry123i <igorsc.jerry@gmail.com>
2022-03-02 13:10:21 -0300

    Border shader color fix

---
+++ Assets/Scripts/ShaderScripts/CameraShaderManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraShaderManager : MonoBehaviour
+{
+    [SerializeField] private ForwardRendererData _rendererData;
+
+    private void Awake()
+    {
+        SetDangerAreaShader(true);
+    }
+
+    public void SetDangerAreaShader(bool value)
+    {
+        if (value)
+        {
+            var a = _rendererData.rendererFeatures;
+        }
+        
+    }
+    
+}
