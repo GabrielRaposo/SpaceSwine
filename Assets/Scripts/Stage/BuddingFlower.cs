f358bc157d3e9e600ea371251379190fd99e23b8
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-05-10 17:19:09 -0300

    Adição da mecânica de Budding Flower

---
+++ Assets/Scripts/Stage/BuddingFlower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddingFlower : MonoBehaviour
+{
+    bool isOpen;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        Round round = GetComponentInParent<Round>();
+        if (round)
+        {
+            round.OnReset += ResetComponents;
+        }
+    }
+
+    private void ResetComponents()
+    {
+        SetState(false);
+    }
+    private void SetState(bool value)
+    {
+        isOpen = value;
+        animator.SetBool("Open", value);
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision) 
+    {
+        if (isOpen)
+            return;
+
+        if (!collision.CompareTag("Player"))
+            return;
+
+        SetState(true);
+    }
+}
