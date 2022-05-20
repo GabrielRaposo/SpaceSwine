764723d22f4f1e735fe612888b34e25428789cf8
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-05-20 14:26:12 -0300

    Importação dos sprites de diálogo da Nave

---
+++ Assets/Scripts/Ship/ShipDialogueBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipDialogueBox : MonoBehaviour
+{
+    const int BASE_WIDTH  = 302;    
+    const int BASE_HEIGHT = 101; 
+
+    [Header("Images")]
+    [SerializeField] Image largeBGImage;
+    [SerializeField] Image smallBGImage;
+
+    [Header("Rect Transforms")]
+    [SerializeField] RectTransform largeBG;
+    [SerializeField] RectTransform smallBG;
+    [SerializeField] RectTransform sideRivets;
+    [SerializeField] RectTransform borderTop;
+    [SerializeField] RectTransform borderBottom;
+
+    void Start()
+    {
+        
+    }
+
+
+}
