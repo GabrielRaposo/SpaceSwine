1e8e985873c65d5fcdd52154680fd1a52695f083
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-05-24 18:21:19 -0300

    Criação do Galaxy Parallax Tester

---
+++ Assets/Scripts/Utility/Trailer 1 Utilities/GalaxyParallaxTester.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class GalaxyParallaxTester : MonoBehaviour
+{
+    [SerializeField] InputAction testInput;
+
+    [Header("Values")]
+    [SerializeField] float duration;
+
+    [Header("Referencias")]
+    [SerializeField] Transform starGroup;
+    [SerializeField] Transform starParticles;
+    [SerializeField] Transform bigStars;
+
+    Sequence sequence;
+
+    private void OnEnable() 
+    {
+        testInput.performed += (ctx) => PlaySequence();
+        testInput.Enable();
+    }
+
+    private void Start() 
+    {
+        PlaySequence();
+    }
+
+    private void PlaySequence()
+    {
+        if (sequence != null)
+            sequence.Kill();
+
+        starGroup.transform.localPosition = Vector3.left * 4.0f;
+        starParticles.transform.localPosition = Vector3.left * 3.0f;
+        bigStars.transform.localPosition = Vector3.left * 9f;
+
+        sequence = DOTween.Sequence();
+        sequence.Append( starGroup.DOLocalMoveX(.5f, duration).SetEase(Ease.Linear) );
+        sequence.Join( starParticles.DOLocalMoveX(3.0f, duration).SetEase(Ease.Linear) );
+        sequence.Join( bigStars.DOLocalMoveX(0, duration).SetEase(Ease.Linear) );
+
+        sequence.SetLoops (-1);
+    }
+
+    private void OnDisable() 
+    {
+        testInput.Disable();
+    }
+}
---
+++ Assets/Scripts/Utility/Trailer 1 Utilities/ScreenShakeCaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ScreenShakeCaller : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera vCam;
+
+    void Start()
+    {
+        
+    }
+}
