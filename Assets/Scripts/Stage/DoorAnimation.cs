3c034820d8a438f4855eb0ac39ee24adb53096c0
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-05-26 17:20:49 -0300

    Base da animação de entrada no portal

--- Assets/Scripts/Stage/DoorAnimation.cs
+++ Assets/Scripts/Stage/DoorAnimation.cs
@@ -10,6 +10,9 @@ public class DoorAnimation : MonoBehaviour
 
     [Header("References")]
     [SerializeField] SpriteRenderer whiteCircle;
+    [SerializeField] Transform visualComponent;
+    [SerializeField] ParticleSystem OnEnterAreaPS;
+    [SerializeField] ParticleSystem CenterBurstPS;
 
     Sequence sequence;
 
@@ -40,6 +43,8 @@ public class DoorAnimation : MonoBehaviour
             player.transform.SetParent(door.transform);
 
             door.SetInteractable(false);
+
+            OnEnterAreaPS?.Play();
         }
 
         sequence = DOTween.Sequence();
@@ -49,12 +54,14 @@ public class DoorAnimation : MonoBehaviour
             {
                 playerCharacter.gameObject.SetActive(false);
                 whiteCircle.enabled = true;
+                CenterBurstPS?.Play();
             }
         );
 
-        sequence.Append
+        sequence.Append( visualComponent.DOPunchScale(Vector3.one * -.1f, duration: .3f) );
+        sequence.Join
         (
-            DOVirtual.Float(from: 1.0f, to: 0.0f, duration: .3f, (f) => whiteCircle.color = new Vector4(1, 1, 1, f))
+            DOVirtual.Float(from: 1.0f, to: 0.0f, duration: .2f, (f) => whiteCircle.color = new Vector4(1, 1, 1, f))
         );
         sequence.AppendInterval(.5f);
 
