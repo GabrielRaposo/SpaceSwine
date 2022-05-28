d8eb9058fd05568faff0433d7e9228e9cbaf3693
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-05-28 19:00:25 -0300

    Implementação de áudio da nave falando

---
+++ Assets/Scripts/Ship/PlaySoundOnType.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RedBlueGames.Tools.TextTyper;
+
+public class PlaySoundOnType : MonoBehaviour
+{
+    [SerializeField] AK.Wwise.Event voiceAkEvent;
+    [SerializeField] TextTyper typer;
+
+    void Start()
+    {
+        if (!typer)
+            typer = GetComponentInChildren<TextTyper>();
+
+        if (!typer || voiceAkEvent == null) 
+        {
+            enabled = false;
+            return;
+        }
+
+    }
+
+    private void Update() 
+    {
+        if (!typer.IsTyping || voiceAkEvent.IsPlaying(gameObject))
+            return;
+
+        voiceAkEvent.Post(gameObject);
+    }
+
+}
