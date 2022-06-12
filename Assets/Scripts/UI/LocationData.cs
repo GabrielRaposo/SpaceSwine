27c7487c7aa1058b267e14b969ad1e34748920f9
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-06-12 20:12:51 -0300

    Adição de Location Display

---
+++ Assets/Scripts/UI/LocationData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationData : MonoBehaviour
+{
+    [SerializeField] string locationCode;
+    [SerializeField] string fallbackText;
+
+    float delay = 1.0f;
+
+    void Start()
+    {
+        LocationDisplay display = LocationDisplay.Instance;
+        if (!display)
+            return;
+
+        string t = LocalizationManager.GetUiText(locationCode, fallbackText);
+
+        RaposUtil.WaitSeconds(this, delay, () => 
+            {
+                display.DisplayLocation(t);
+            }
+        );
+    }
+
+}
