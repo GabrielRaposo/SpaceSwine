17f72904e21a5419590dc70c3df26cf34f678592
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-06-14 19:16:15 -0300

    Efeito de Hit temporário adicionado

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class Health : MonoBehaviour
 {
     [SerializeField] int startingValue;
+    [SerializeField] ParticleSystem damageFX;
     [SerializeField] AK.Wwise.Event deathAKEvent;
 
     int value;
@@ -26,12 +27,34 @@ public class Health : MonoBehaviour
         if (!hitbox)
             return;
 
-        TakeDamage(hitbox.damage);
+        Vector2 point = transform.position;
+
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, .5f, Vector2.zero);
+        if (hit.Length > 0 )
+        {
+            foreach(RaycastHit2D h in hit)
+            {
+                Hitbox testBox = h.collider.GetComponent<Hitbox>();
+                if (testBox)
+                {
+                    //Debug.Log (h.point);
+                    point = h.point;
+                    break;
+                }
+            }
+        }
+
+        TakeDamage(hitbox.damage, point);
     }
 
-    private void TakeDamage(int damage)
+    private void TakeDamage(int damage, Vector2 damagePosition)
     {
         Debug.Log($"<color=#dd0000><b>Player Take damage</b></color>");
+        if (damageFX)
+        {
+            damageFX.transform.position = damagePosition;
+            damageFX.Play();
+        }
         value -= damage;
 
         if (value < 0)
