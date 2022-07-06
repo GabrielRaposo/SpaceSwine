fdeba6928e78f3da61f356729fb858215125b727
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-07-05 21:19:17 -0300

    Base de Hazard Electric Mind

---
+++ Assets/Scripts/Mechanics/ElectricLine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricLine : MonoBehaviour
+{   
+    bool autoUpdate;
+
+    Transform start; 
+    Transform end; 
+
+    SpriteRenderer sr;
+    BoxCollider2D coll;
+
+    private void Awake() 
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        coll = GetComponentInChildren<BoxCollider2D>();    
+    }
+
+    public void Setup (bool autoUpdate, Transform start, Transform end)
+    {
+
+        this.autoUpdate = autoUpdate;
+
+        this.start = start;
+        this.end   = end;
+
+        UpdateLine();
+    }
+
+    private void Update() 
+    {
+        if (!autoUpdate)
+            return;
+
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        transform.position = start.transform.position;
+        
+        Vector2 direction = end.position - start.position;
+        if (sr) 
+            sr.size = new Vector2(direction.magnitude, sr.size.y);
+        if (coll)
+        {
+            coll.offset = new Vector2(direction.magnitude/2, 0);
+            coll.size = new Vector2(direction.magnitude, coll.size.y);
+        }
+        transform.eulerAngles = Vector3.forward * (Vector2.SignedAngle(Vector2.right, direction.normalized));
+    }
+}
---
+++ Assets/Scripts/Mechanics/ElectricMind.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricMind : MonoBehaviour
+{
+    [SerializeField] bool loop;
+    [SerializeField] bool autoUpdate;
+    [SerializeField] ElectricLine electricLine;
+    [SerializeField] Transform linesGroup;
+
+    ElectricBall[] electricballs;
+
+    private void Start() 
+    {
+        if (electricLine == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        electricballs = GetComponentsInChildren<ElectricBall>();
+        if (electricballs.Length < 2)
+        {
+            gameObject.SetActive(false);
+            return;    
+        }
+
+        SetupChain();
+    }
+
+    private void SetupChain()
+    {
+        int max = electricballs.Length - (loop ? 0 : 1); 
+        for (int i = 0; i < max; i++) 
+        {
+            GameObject lineObject = Instantiate (electricLine.gameObject, linesGroup);
+            
+            ElectricLine lineScript = lineObject.GetComponent<ElectricLine>();
+            if (lineScript) lineScript.Setup(autoUpdate, BallAt(i+1).transform, BallAt(i).transform);
+        }
+
+        electricLine.gameObject.SetActive(false);
+    }
+
+    ElectricBall BallAt (int index)
+    {
+        return electricballs[index % electricballs.Length];
+    }
+}
