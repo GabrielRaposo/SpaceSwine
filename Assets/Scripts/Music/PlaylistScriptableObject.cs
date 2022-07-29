cab528253b8266cdc69e9f77d5356a67f0a3097a
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-07-29 16:49:43 -0300

    Adição de ReordetableList no projeto

---
+++ Assets/Scripts/Music/PlaylistScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Malee.List;
+
+[CreateAssetMenu (fileName = "Playlist", menuName = "ScriptableObjects/Playlist Data") ]
+public class PlaylistScriptableObject : ScriptableObject
+{
+	//[Reorderable(paginate = true, pageSize = 2)]
+	[Reorderable] public TrackList list;
+}
+
+[System.Serializable]
+public class TrackList : ReorderableArray<MusicDataScriptableObject> {
+}
