3911590f3fdb884b17f1792c5824dbfc5f99277c
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-08-06 20:52:52 -0300

    Base de chamada da cena de Intro

--- Assets/Scripts/UI/TitleScreen/TitleScreenFunctions.cs
+++ Assets/Scripts/UI/TitleScreen/TitleScreenFunctions.cs
@@ -21,7 +21,8 @@ public class TitleScreenFunctions : MonoBehaviour
     {
         //Debug.Log("Play");
 
-        SceneTransition.LoadScene((int)BuildIndex.World0Exploration, SceneTransition.TransitionType.WhiteFade);
+        TrailerSceneCaller.AutoStart = true;
+        SceneTransition.LoadScene((int)BuildIndex.Ship, SceneTransition.TransitionType.WhiteFade);
         //SceneTransition.LoadScene((int) BuildIndex.World1Exploration, SceneTransition.TransitionType.WhiteFade );
     }
 
