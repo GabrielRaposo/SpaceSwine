0c6c282ca607f07ca187abe21a9f5bc9f8e219b5
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-08-07 16:05:04 -0300

    Base de Warnings

---
+++ Assets/Scripts/Utility/RescaleTween.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RescaleTween : MonoBehaviour
+{
+    [SerializeField] Vector2 startScale;
+    [SerializeField] Vector2 targetScale;
+    [SerializeField] float duration;
+
+    void Start()
+    {
+        transform.localScale = startScale;
+    }
+
+    public void Call()
+    {
+        transform.DOScale(targetScale, duration);
+    }
+}
