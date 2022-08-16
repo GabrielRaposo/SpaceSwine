421b21dba0ea7a3568100974dbe4f1f1e2e69a91
Jerry123i <igorsc.jerry@gmail.com>
2022-08-16 11:38:57 -0700

    border wave fix (#82)

--- Assets/Shaders/CameraShader/DistortedBorders.hlsl
+++ Assets/Shaders/CameraShader/DistortedBorders.hlsl
@@ -7,10 +7,10 @@ void DistortedBorder_float(float x, float amplitude, float frequency,float time,
     //float frequency = 1.;
     y = sin(x * frequency);
     float t = 0.01*(-time*130.0);
-    y += sin(x*frequency*frequencies.x + t*timeFactors.x)*weight.x;
-    y += sin(x*frequency*frequencies.y + t*timeFactors.y)*weight.y;
-    y += sin(x*frequency*frequencies.z + t*timeFactors.z)*weight.z;
-    y += sin(x*frequency*frequencies.w + t*timeFactors.w)*weight.w;
+    // y += sin(x*frequency*frequencies.x + t*timeFactors.x)*weight.x;
+    // y += sin(x*frequency*frequencies.y + t*timeFactors.y)*weight.y;
+    // y += sin(x*frequency*frequencies.z + t*timeFactors.z)*weight.z;
+    // y += sin(x*frequency*frequencies.w + t*timeFactors.w)*weight.w;
     y *= amplitude*0.03;   
     
 }
