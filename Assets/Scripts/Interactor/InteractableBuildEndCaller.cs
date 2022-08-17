49d6a3c4f52fbaab605551c3203fa0a562d94412
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-08-17 19:05:28 -0300

    Fade out na cena de end build

---
+++ Assets/Scripts/Interactor/InteractableBuildEndCaller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBuildEndCaller : Interactable
+{
+    [SerializeField] GameObject inputIcon;
+
+    private void Start() 
+    {
+        HighlightState(false);
+    }
+
+    public override void Interaction(PlayerInteractor interactor) 
+    {
+        base.Interaction(interactor);
+
+        Debug.Log("Hi icon");
+    }
+
+    protected override void HighlightState(bool value) 
+    {
+        if (inputIcon)
+            inputIcon.SetActive(value);
+    }
+
+}
