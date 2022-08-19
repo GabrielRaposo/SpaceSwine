ed76f82fd4eb1c080db4cdadbac9c3d9e969e9c9
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-08-19 18:45:31 -0300

    Portal secreto removido

--- Assets/Scripts/Stage/AttachToPlanet.cs
+++ Assets/Scripts/Stage/AttachToPlanet.cs
@@ -16,15 +16,15 @@ public class AttachToPlanet : MonoBehaviour
         Attach();
     }
 
-    #if UNITY_EDITOR
     protected virtual void Update() 
     {
+        #if UNITY_EDITOR
         if (Application.isPlaying)
             return;
 
         Attach();
+        #endif
     }
-    #endif
 
     protected void Attach()
     {
