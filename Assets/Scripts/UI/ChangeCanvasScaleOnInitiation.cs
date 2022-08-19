1ff14ef831d4921c2eec2f9f5be49cb468be52d3
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-08-19 12:51:07 -0300

    ChangeCanvasScaleOnInitiation importado do SBM

---
+++ Assets/Scripts/UI/ChangeCanvasScaleOnInitiation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class ChangeCanvasScaleOnInitiation : MonoBehaviour
+{
+    CanvasScaler canvasScaler;
+
+    void Start()
+    {
+        canvasScaler = GetComponent<CanvasScaler>();
+        if (!canvasScaler)
+            return;
+
+        float defaultRatio = 16f / 9f;
+        float currentRatio = Screen.width / Screen.height;
+
+        if (currentRatio > defaultRatio)
+        {
+            canvasScaler.matchWidthOrHeight = 1;
+        }
+        else
+        {
+            canvasScaler.matchWidthOrHeight = 0;
+        }
+    }
+}
