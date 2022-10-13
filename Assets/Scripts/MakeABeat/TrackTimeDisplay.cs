85944c0e7a209be0d3564790d67948ae35d7e9ab
GabrielRaposo <gabriel.s.raposo@gmail.com>
2022-10-13 17:17:07 -0300

    Contador de BPM visual

---
+++ Assets/Scripts/MakeABeat/TrackTimeDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MakeABeat
+{
+    public class TrackTimeDisplay : MonoBehaviour
+    {
+        [SerializeField] Image fillDisplay; 
+
+        void Start()
+        {
+            BeatMaker beatMaker = GetComponentInParent<BeatMaker>();
+            if (!beatMaker)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            beatMaker.UpdateDisplayAction  += UpdateDisplay;
+            beatMaker.SignaturePulseAction += PulseImage;
+        }
+
+        void UpdateDisplay (float fillAmount)
+        {
+            if (!fillDisplay)
+                return;
+
+            fillDisplay.fillAmount = 1 - fillAmount;
+        }
+
+        void PulseImage(int step)
+        {
+            
+        }
+        
+    }
+}
