ab1cbdd360fcb0966b9a93451647c79d99e658c6
GabrielRaposo <gabriel.s.raposo@gmail.com>
2023-02-28 13:07:12 -0300

    Ajustes para a build

--- Assets/Scripts/Character/SetCameraShaderOnStart.cs
+++ Assets/Scripts/Character/SetCameraShaderOnStart.cs
@@ -6,7 +6,7 @@ public class SetCameraShaderOnStart : MonoBehaviour
 {
     [SerializeField] LocalGameplayState gameplayState;
 
-    void Start()
+    void Awake()
     {
         if (!gameplayState)
             return;
