cc61c81e90fb9dc61f844e3a63e7b9b23e7cbd18
Jerry123i <igorsc.jerry@gmail.com>
2023-03-13 13:24:34 -0300

    Terminal stages

--- Assets/Editor/CircleRulerEditor.cs
+++ Assets/Editor/CircleRulerEditor.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(CircleRuler))]
+[CustomEditor(typeof(CircleRuler)), CanEditMultipleObjects]
 public class CircleRulerEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         var obj = (CircleRuler)target;
 
+        GUILayout.BeginHorizontal();
+
         if (GUILayout.Button("Get children"))
             obj.GetAllChildren();
+
+        if (GUILayout.Button("+", GUILayout.Width(30)))
+        {
+            var child = obj.objects[0];
+            var newChild = Instantiate(child, child.parent);
+            obj.objects.Add(newChild);
+            obj.OnValidate();
+        }
+        
+        GUILayout.EndHorizontal();
         
         EditorGUILayout.Space(10);
         
---
+++ Assets/Scripts/Terminal/TerminalGenericCall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TerminalGenericCall : MonoBehaviour, ITerminalEvent
+{
+    public UnityEvent action;
+    
+    // public GameObject parent;
+    // [SerializeField] private List<MonoBehaviour> monoBehaviours;
+    // [SerializeField] private int selectionIndex;
+
+    public void Activate(InteractableTerminal terminal, PlayerInteractor interactor)
+    {
+        if(action!=null)
+            action.Invoke();
+    }
+}
