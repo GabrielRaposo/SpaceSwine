378773401c42ce87ad4afcecedc0ee2a757ae2a5
Jerry123i <igorsc.jerry@gmail.com>
2023-04-07 01:47:33 -0300

    Lock terminal animation e Level fixes

--- Assets/Scripts/Stage/Lock.cs
+++ Assets/Scripts/Stage/Lock.cs
@@ -20,11 +20,13 @@ public class Lock : MonoBehaviour
 
     private Door _door;
     private Round _round;
-    private Animator animator;
+    [SerializeField]private Animator animator;
 
     public void Init (Door s, bool isInternal)
     {
-        animator = GetComponent<Animator>();
+        if(animator == null)
+            animator = GetComponent<Animator>();
+        
         _door = s;
 
         health = startingHealth;
