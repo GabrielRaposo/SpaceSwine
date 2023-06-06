362e8a153f1f7ee4c3c9b99a75b8b9a9a11d895c
GabrielRaposo <gabriel.s.raposo@gmail.com>
2023-06-06 16:24:49 -0300

    Adição do prefab Persistent Steam Manager na Splash Screen

---
+++ Assets/Scripts/Utility/SteamWorksSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamWorksSingleton : MonoBehaviour
+{
+    static SteamWorksSingleton Instance;
+
+    private void Awake() 
+    {
+        if (Instance)
+        {
+            Destroy (gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad (gameObject);
+        Instance = this;
+    }
+}
