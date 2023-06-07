3d38cf8f9f0113644b543939e9f79a30342d771d
GabrielRaposo <gabriel.s.raposo@gmail.com>
2023-06-06 21:34:16 -0300

    Base de código de Adventure Log Tab

---
+++ Assets/Scripts/AdventureLog/AdventureLogDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AdventureLogDisplay : MonoBehaviour
+{
+    [SerializeField] Transform logsParent;
+    [SerializeField] TextMeshProUGUI labelDisplay;
+    [SerializeField] AdventureLogTab baseTab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
