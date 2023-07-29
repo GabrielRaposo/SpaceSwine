96d871857f7f54a16075aa53ff112adf5d7c3655
GabrielRaposo <gabriel.s.raposo@gmail.com>
2023-07-29 14:51:25 -0300

    Ajustes locais

---
+++ Assets/Scripts/AdventureLog/LogWaypoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogWaypoint : MonoBehaviour
+{
+    [SerializeField] float hideRadius;
+    [SerializeField] List<AdventureLogScriptableObject> logs;
+
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        
+    }
+}
