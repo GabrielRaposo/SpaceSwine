54b1953b3376d553e013a198cd84da1760405775
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-01-10 17:51:11 -0300

    Ajustes locais

---
+++ Assets/Scripts/SpaceNavigation/NavigationInteractableGlimmer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationInteractableGlimmer : MonoBehaviour
+{
+    public float spinMultiplier;
+    public float spinDuration;
+
+    float spinCount;
+    int direction = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (spinCount > 0)
+            return;
+
+        NavigationShip ship = collision.GetComponent<NavigationShip>();
+        if (!ship) 
+            return;
+
+        spinCount = spinDuration;
+        direction = Random.Range(0, 2) == 0 ? 1 : -1;  
+    }
+
+    private void Start()
+    {
+        transform.Rotate (Vector3.forward * Random.Range(0, 45));
+    }
+
+    private void Update()
+    {
+        if (spinCount < 0)
+            return;
+
+        transform.Rotate ( Vector3.forward * (spinMultiplier * (spinCount/spinDuration) * direction) );
+        spinCount -= Time.deltaTime;
+    }
+}
