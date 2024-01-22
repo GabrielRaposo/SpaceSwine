21951a3f67f523400966e697673a015e309a1445
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-01-22 19:54:29 -0300

    Pequenos ajustes

---
+++ Assets/Scripts/Navigation/NavigationEyePlanet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class NavigationEyePlanet : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine( WaitToCallAnimation() );
+    }
+
+    IEnumerator WaitToCallAnimation()
+    {
+        while (true) 
+        {
+            float random = Random.Range(5f, 40f);
+            yield return new WaitForSeconds( random );
+
+            animator.SetTrigger("Show");
+        }
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
