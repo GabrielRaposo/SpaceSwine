79fe3c88554c89457182520c0c4c08f6f290b405
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-02-23 23:31:45 -0300

    Implementação do sistema da tela de Ending Cutscene e criação de Tela de Créditos

---
+++ Assets/Scripts/UI/EndingCutsceneCaller.cs
@@ -0,0 +1,155 @@
+﻿using DevLocker.Utils;
+using RedBlueGames.Tools.TextTyper;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class EndingCutsceneCaller : MonoBehaviour
+{   
+    [SerializeField] float frameTransitionDuration;
+    [SerializeField] List<CutsceneStoryData> cutsceneStoryDatas; 
+    [SerializeField] SceneReference creditsSceneReference;
+
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI mainTextDisplay;
+    [SerializeField] TextTyper mainTextTyper;
+    [SerializeField] Image imageDisplay;
+    [SerializeField] Image proceedIcon;
+    [SerializeField] CanvasGroup imageCanvasGroup;
+
+    int frameIndex;
+    int textIndex;
+    bool typeCompletedTrigger;
+    bool readingInput;
+
+    PlayerInputActions inputActions;
+    Sequence s;
+
+    private void OnEnable()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+
+        inputActions.UI.Confirm.performed += (ctx) => 
+        {
+            if (!readingInput)
+                return;
+            
+            ProceedInput();
+        };
+    }
+
+    void Start()
+    {
+        // -- Desligar todos os outros componentes de UI  
+
+
+        // -- Setups
+        proceedIcon.enabled = false;
+        mainTextDisplay.text = string.Empty;
+        imageDisplay.sprite = null;
+        imageCanvasGroup.alpha = 0;
+
+        frameIndex = textIndex = 0;
+
+        StartCoroutine ( Call() );
+    }
+
+    void ProceedInput()
+    {
+        StartCoroutine( Proceed() );
+    }
+
+    IEnumerator Call()
+    {
+        yield return new WaitForSeconds( 2f );
+        yield return Proceed();
+    }
+
+    IEnumerator Proceed() 
+    {
+        readingInput = false;
+        proceedIcon.enabled = false;
+
+        mainTextDisplay.text = string.Empty;
+        
+        if (frameIndex >= cutsceneStoryDatas.Count)
+        {
+            yield return EndCutscene();
+            yield break;
+        }
+
+        CutsceneStoryData cutsceneStoryData = cutsceneStoryDatas[frameIndex % cutsceneStoryDatas.Count];
+
+        bool done = false;
+        if (textIndex < 1) // -- Mostra o frame
+        {
+            // mostra imagem primeiro, depois escreve o texto
+            imageCanvasGroup.alpha = 0;
+            imageDisplay.sprite = cutsceneStoryData.frameSprite;
+
+            done = false;
+            s = DOTween.Sequence();
+            s.Append( imageCanvasGroup.DOFade(1f, frameTransitionDuration) );
+            s.OnComplete(() => done = true );
+
+            yield return new WaitUntil( () => done );
+        }
+
+        yield return new WaitForSeconds(.2f);
+
+        (bool isValid, string text) data = LocalizationManager.GetStoryText(cutsceneStoryData.textTags[textIndex % cutsceneStoryData.textTags.Count]);
+        if (!data.isValid)
+        {
+            DebugDisplay.Call("Invalid text tag");
+            yield break;
+        }
+
+        typeCompletedTrigger = false;
+        mainTextTyper.TypeText(data.text);
+        yield return new WaitUntil( () => typeCompletedTrigger );
+
+        // -- Advance Indexes
+        textIndex++;
+        if (textIndex >= cutsceneStoryData.textTags.Count)
+        {
+            textIndex = 0;
+            frameIndex++;
+        }
+
+        readingInput = true;
+        proceedIcon.enabled = true;
+    }
+
+    IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds(1f);
+        DebugDisplay.Call("Cutscene ended");
+
+        s = DOTween.Sequence();
+        s.Append( imageCanvasGroup.DOFade(0f, duration: 3f) );
+        s.AppendInterval(2f);
+
+        s.OnComplete( () => SceneTransition.LoadScene(creditsSceneReference.ScenePath, SceneTransition.TransitionType.BlackFade) );
+    }
+
+    public void OnTypeCompleted()
+    {
+        typeCompletedTrigger = true;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+}
+
+[System.Serializable]
+public class CutsceneStoryData
+{
+    public Sprite frameSprite;
+    public List<string> textTags;
+}
