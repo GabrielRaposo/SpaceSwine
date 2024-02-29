32baa73659b9a9d99e31f349783ff8205e7b534b
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-02-29 17:09:29 -0300

    Adição de Post Processing na cena final

--- Assets/Scripts/SoundtrackManager.cs
+++ Assets/Scripts/SoundtrackManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using DG.Tweening;
-using UnityEditor.ShaderGraph.Internal;
 
 public class SoundtrackManager : MonoBehaviour
 {
