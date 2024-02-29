c9462f0805b4566cf07d480835b3dde692a9fad6
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-02-29 12:14:40 -0300

    Base de chamada de texto imediata da nave

---
+++ Assets/Scripts/Interactor/CallInteractableNPCOnContact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallInteractableNPCOnContact : MonoBehaviour
+{
+    [SerializeField] float waitTime;
+    [SerializeField] InteractableNPCSimplified NPC;
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (NPC == null)
+            return;
+
+        PlayerInteractor player = collision.GetComponent<PlayerInteractor>();
+        if (!player)
+            return;
+        
+        NPC.StartCoroutine(BlockPlayerAndCallDialogue(player));
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator BlockPlayerAndCallDialogue(PlayerInteractor player)
+    {
+        NPC.Interaction(player);
+        //NPC.Interaction(player);
+        yield break;
+    }
+}
