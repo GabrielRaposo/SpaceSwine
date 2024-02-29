bc7bee2eb7883e65b20357aeb9cd0f1c58faee5b
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-02-29 20:10:44 -0300

    Ajustes de Playlists nas cenas finais

---
+++ Assets/Scripts/UI/TitleScreen/ForcePlaylist.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePlaylist : MonoBehaviour
+{
+    [SerializeField] PlaylistScriptableObject playlist;
+
+    void Start()
+    {
+        if (playlist == null)
+            return;
+
+        if (SoundtrackManager.Instance)
+            SoundtrackManager.Instance.ForceSkipToPlaylist(playlist);
+    }
+}
