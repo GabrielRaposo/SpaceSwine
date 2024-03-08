1def0a0ff878635825017b645f8e74a82f4cc12b
GabrielRaposo <gabriel.s.raposo@gmail.com>
2024-03-07 22:57:04 -0300

    Base simples do minigame Traveler

---
+++ Assets/Scripts/Minigame/Traveler/MT_Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MT_Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform aimAnchor;
+
+    Vector2 direction;
+    Rigidbody2D rb;
+    MT_ScreenLooper screenLooper;
+
+    PlayerInputActions inputActions;
+    InputAction movementAction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        screenLooper = GetComponent<MT_ScreenLooper>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+
+        movementAction = inputActions.Minigame.Movement;
+        movementAction.Enable();
+
+        inputActions.Minigame.Action.performed += (ctx) => 
+        {
+            ShootInput();
+        };
+        inputActions.Minigame.Action.Enable();
+    }
+
+    void Start()
+    {
+        direction = Vector2.up;
+        UpdateAimDisplay();
+    }
+
+    void UpdateAimDisplay()
+    {
+        if (!aimAnchor || !screenLooper.InsideZone)
+            return;
+
+        aimAnchor.eulerAngles = Vector3.forward * Vector2.SignedAngle(Vector2.up, direction);
+    }
+
+    void ShootInput()
+    {
+        rb.velocity = speed * direction * -1;
+    }
+
+    void Update()
+    {
+        Vector2 axisInput = movementAction.ReadValue<Vector2>();
+        
+        if (axisInput == Vector2.zero)
+            return;
+
+        direction = axisInput.normalized;
+        UpdateAimDisplay();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+
+        movementAction.Disable();
+        inputActions.Minigame.Action.Disable();
+    }
+}
---
+++ Assets/Scripts/Minigame/Traveler/MT_ScreenLooper.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MT_ScreenLooper : MonoBehaviour
+{
+    [SerializeField] float threshold = .5f;
+
+    const int SCREEN_X = 10;
+    const int SCREEN_Y = 9;
+
+    [HideInInspector] public bool InsideZone;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GameplayArea"))
+        {
+            InsideZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (InsideZone && collision.CompareTag("GameplayArea"))
+        {
+            InsideZone = false;
+            ScreenLoop();
+        }
+    }
+
+    private void Update()
+    {
+        if (InsideZone)
+            return;
+
+        ScreenLoop();
+    }
+
+    private void ScreenLoop()
+    {
+        //Debug.Log("transform.localPosition: " + transform.localPosition);
+
+        if (transform.localPosition.x - threshold > SCREEN_X / 2)
+        {
+            Debug.Log("A");
+            transform.position += Vector3.left * (SCREEN_X + (threshold * 2));
+            //insideZone = true;
+            return;
+        }
+
+        if (transform.localPosition.x + threshold < -SCREEN_X / 2)
+        {
+            Debug.Log("B");
+            transform.position += Vector3.right * (SCREEN_X + (threshold * 2));
+            //insideZone = true;
+            return;
+        }
+
+        if (transform.localPosition.y - threshold > SCREEN_Y / 2)
+        {
+            transform.position += Vector3.down * (SCREEN_Y + (threshold * 2));
+            //insideZone = true;
+            return;
+        }
+
+        if (transform.localPosition.y + threshold < -SCREEN_Y / 2)
+        {
+            transform.position += Vector3.up * (SCREEN_Y + (threshold * 2));
+            //insideZone = true;
+            return;
+        }
+    }
+}
